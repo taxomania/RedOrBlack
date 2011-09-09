94e850a92418d5dfea2562f6f9003a3d505de29b
Tariq Patel <tariq.patel@gmail.com>
2011-09-09 21:52:02 +0100

    Refactored layout initialisation

--- src/taxomania/games/redorblack/RedOrBlackActivity.java
+++ src/taxomania/games/redorblack/RedOrBlackActivity.java
@@ -15,7 +15,7 @@ import android.widget.TextView;
 public class RedOrBlackActivity extends FragmentActivity {
     private static final int MAIN_LAYOUT_ID = Integer.MAX_VALUE;
     private static final int TEXT_SIZE = 30;
-    private static final int PADDING = 5;
+    private static final int PADDING = 10;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,32 +37,23 @@ public class RedOrBlackActivity extends FragmentActivity {
         final LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT,
                 LayoutParams.WRAP_CONTENT);
         final float textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE, dm);
-        TextView tv = new TextView(this);
-        tv.setText("RED");
-        tv.setTextColor(Color.RED);
-        tv.setTextSize(textSize);
-        tv.setLayoutParams(lp);
-        tv.setGravity(Gravity.LEFT);
-        mainLayout.addView(tv);
-
-        tv = new TextView(this);
-        tv.setText("OR");
-        tv.setTextColor(Color.WHITE);
-        tv.setTextSize(textSize);
-        tv.setLayoutParams(lp);
-        tv.setGravity(Gravity.CENTER);
-        mainLayout.addView(tv);
-
-        tv = new TextView(this);
-        tv.setText("BLACK");
-        tv.setTextColor(Color.BLACK);
-        tv.setTextSize(textSize);
-        tv.setLayoutParams(lp);
-        tv.setGravity(Gravity.RIGHT);
-        mainLayout.addView(tv);
+        mainLayout.addView(makeHeaderText("RED", Color.RED, textSize, Gravity.LEFT, lp));
+        mainLayout.addView(makeHeaderText("OR", Color.WHITE, textSize, Gravity.CENTER, lp));
+        mainLayout.addView(makeHeaderText("BLACK", Color.BLACK, textSize, Gravity.RIGHT, lp));
 
         return mainLayout;
     } // setLayout()
+
+    private TextView makeHeaderText(final String text, final int color, final float textSize,
+            final int gravity, final LayoutParams lp) {
+        final TextView tv = new TextView(this);
+        tv.setText(text);
+        tv.setTextColor(color);
+        tv.setTextSize(textSize);
+        tv.setGravity(gravity);
+        tv.setLayoutParams(lp);
+        return tv;
+    } // makeHeaderText(String, int, float, int, LayoutParams)
 
     void loseGame() {
         replaceFragment(new LoseGameFragment());
